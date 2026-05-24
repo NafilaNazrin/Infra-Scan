86abc114b5db6842467529622d23a68dd60f6210
NafilaNazrin <144591263+NafilaNazrin@users.noreply.github.com>
2026-05-24 23:36:43 +0530

    Fix version parsing for wandb in logger

--- yolov5/utils/loggers/__init__.py
+++ yolov5/utils/loggers/__init__.py
@@ -6,6 +6,7 @@ import os
 import warnings
 from pathlib import Path
 
+
 import importlib.metadata as pkg
 import torch
 
@@ -31,7 +32,7 @@ try:
     import wandb
 
     assert hasattr(wandb, "__version__")  # verify package import not local dir
-    if pkg.parse_version(wandb.__version__) >= pkg.parse_version("0.12.2") and RANK in {0, -1}:
+    if version.parse(wandb.__version__) >= version.parse("0.12.2") and RANK in {0, -1}:
         try:
             wandb_login_success = wandb.login(timeout=30)
         except wandb.errors.UsageError:  # known non-TTY terminal issue
